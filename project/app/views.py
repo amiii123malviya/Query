8ef49097be37c45136480c0a12f89b27dbee4540
amisha <amishamalviya2607@gmail.com>
2024-04-06 22:30:31 +0530

    queryadd

--- project/app/views.py
+++ project/app/views.py
@@ -12,8 +12,68 @@ from django.http import HttpResponse
 
 
 def home(request):
-    data=Student.objects.all()
-    data1=data.values()
-    return HttpResponse(data1)
-    print(data)
+    # data=Student.objects.all()
+    # data1=data.values()
+    # return HttpResponse(data1)
+    # print(data)
     # return HttpResponse(data)
+
+
+#filter()
+    # data=Student.objects.filter(stu_city='bhopal')
+    # data1=data.values()
+    # return HttpResponse(data1)
+
+
+#exclude()
+    # data=Student.objects.exclude(stu_city='bhopal')
+    # data1=data.values()
+    # return HttpResponse(data1)
+
+#order_by()
+    # data=Student.objects.order_by('stu_city')#in assending order
+    # data1=data.values()
+    # return HttpResponse(data1)
+
+    # data=Student.objects.order_by('-stu_city')#in decending order
+    # data1=data.values()
+    # return HttpResponse(data1)
+
+    # data=Student.objects.order_by('?')#it will arrange in random order each time you refersh server order
+    # data1=data.values()
+    # return HttpResponse(data1)
+
+    # data=Student.objects.order_by('stu_name')#in assending order names
+    # data1=data.values()
+    # return HttpResponse(data1)
+
+#reverse()
+    # data=Student.objects.order_by('id').reverse()#accordiing to id it will reverse
+    # data=Student.objects.order_by('id').reverse()[:2]#first 3 entries
+    # data1=data.values()
+    # return HttpResponse(data1)
+
+    #values()=>it will provides the dict formated value
+    # data=Student.objects.values()#in assending order
+    # # data1=data.values()
+    # data=Student.objects.values('stu_name','stu_city')#in assending order
+
+    # return HttpResponse(data)
+
+    #values_list(*fields,flat=False,named=false)=>it will provide the value in tuple format
+
+    # data=Student.objects.values_list()#in assending order
+    # data=Student.objects.values_list('id','stu_name')#in assending order
+    data=Student.objects.values_list('id','stu_name',named=True)#in assending order
+
+    return HttpResponse(data)
+
+
+
+    
+    
+ 
+
+
+    
+
